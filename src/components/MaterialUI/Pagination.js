ab10729d422b194de6f6d484a9cbbcfa7addeb4e
Britt Mo <brittany.dunne@utexas.edu>
2019-01-26 14:35:33 -0700

    pagination UI buttons changed

--- src/components/MaterialUI/Pagination.js
+++ src/components/MaterialUI/Pagination.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import Icon from '@material-ui/core/Icon';
@@ -10,47 +9,49 @@ const styles = theme => ({
     },
     extendedIcon: {
         marginRight: theme.spacing.unit,
-    },
+    }
 });
 
 class Pagination extends React.Component {
     render() {
         const { classes, handlePagination, page } = this.props;
         return (
-            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '15px' }}>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <Fab onClick={() => handlePagination('previous')} color="default" size="small" aria-label="Previous" className={classes.fab}>
                     <Icon>navigate_before</Icon>
                 </Fab>
-                <Fab onClick={() => handlePagination(1)} color={page === 1 ? "primary" : "default"} size="small" aria-label="Previous" className={classes.fab}>
-                    1
-                </Fab>
-                <Fab onClick={() => handlePagination(2)} color={page === 2 ? "primary" : "default"} size="small" aria-label="Previous" className={classes.fab}>
-                    2
-                </Fab>
-                <Fab onClick={() => handlePagination(3)} color={page === 3 ? "primary" : "default"} size="small" aria-label="Previous" className={classes.fab}>
-                    3
-                </Fab>
-                <Fab onClick={() => handlePagination(4)} color={page === 4 ? "primary" : "default"} size="small" aria-label="Previous" className={classes.fab}>
-                    4
-                </Fab>
-                <Fab onClick={() => handlePagination(5)} color={page === 5 ? "primary" : "default"} size="small" aria-label="Previous" className={classes.fab}>
-                    5
-                </Fab>
-                <Fab onClick={() => handlePagination(6)} color={page === 6 ? "primary" : "default"} size="small" aria-label="Previous" className={classes.fab}>
-                    6
-                </Fab>
-                <Fab onClick={() => handlePagination(7)} color={page === 7 ? "primary" : "default"} size="small" aria-label="Previous" className={classes.fab}>
-                    7
-                </Fab>
-                <Fab onClick={() => handlePagination(8)} color={page === 8 ? "primary" : "default"} size="small" aria-label="Previous" className={classes.fab}>
-                    8
-                </Fab>
-                <Fab onClick={() => handlePagination(9)} color={page === 9 ? "primary" : "default"} size="small" aria-label="Previous" className={classes.fab}>
-                    9
-                </Fab>
-                <Fab onClick={() => handlePagination(10)} color={page === 10 ? "primary" : "default"} size="small" aria-label="Previous" className={classes.fab}>
-                    10
-                </Fab>
+                <div style={{ paddingTop: '23px' }}>
+                    <a href style={{ cursor: 'pointer', fontWeight: page === 1 ? 'bold' : 'normal' }} onClick={() => handlePagination(1)} size="small" aria-label="Previous" className={classes.fab}>
+                        1
+                    </a>
+                    <a href style={{ cursor: 'pointer', fontWeight: page === 2 ? 'bold' : 'normal' }} onClick={() => handlePagination(2)} size="small" aria-label="Previous" className={classes.fab}>
+                        2
+                    </a>
+                    <a href style={{ cursor: 'pointer', fontWeight: page === 3 ? 'bold' : 'normal' }} onClick={() => handlePagination(3)} size="small" aria-label="Previous" className={classes.fab}>
+                        3
+                    </a>
+                    <a href style={{ cursor: 'pointer', fontWeight: page === 4 ? 'bold' : 'normal' }} onClick={() => handlePagination(4)} size="small" aria-label="Previous" className={classes.fab}>
+                        4
+                    </a>
+                    <a href style={{ cursor: 'pointer', fontWeight: page === 5 ? 'bold' : 'normal' }} onClick={() => handlePagination(5)} size="small" aria-label="Previous" className={classes.fab}>
+                        5
+                    </a>
+                    <a href style={{ cursor: 'pointer', fontWeight: page === 6 ? 'bold' : 'normal' }} onClick={() => handlePagination(6)} size="small" aria-label="Previous" className={classes.fab}>
+                        6
+                    </a>
+                    <a href style={{ cursor: 'pointer', fontWeight: page === 7 ? 'bold' : 'normal' }} onClick={() => handlePagination(7)} size="small" aria-label="Previous" className={classes.fab}>
+                        7
+                    </a>
+                    <a href style={{ cursor: 'pointer', fontWeight: page === 8 ? 'bold' : 'normal' }} onClick={() => handlePagination(8)} size="small" aria-label="Previous" className={classes.fab}>
+                        8
+                    </a>
+                    <a href style={{ cursor: 'pointer', fontWeight: page === 9 ? 'bold' : 'normal' }} onClick={() => handlePagination(9)} size="small" aria-label="Previous" className={classes.fab}>
+                        9
+                    </a>
+                    <a href style={{ cursor: 'pointer', fontWeight: page === 10 ? 'bold' : 'normal' }} onClick={() => handlePagination(10)} size="small" aria-label="Previous" className={classes.fab}>
+                        10
+                    </a>
+                </div>
                 <Fab onClick={() => handlePagination('next')} color="default" size="small" aria-label="Next" className={classes.fab}>
                     <Icon>navigate_next</Icon>
                 </Fab>
@@ -58,10 +59,5 @@ class Pagination extends React.Component {
         )
     }
 }
-
-
-Pagination.propTypes = {
-    // classes: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles)(Pagination);
